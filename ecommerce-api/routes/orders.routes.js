41fced29fb8df2c4ddb513e54b2bedbf801169c7
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-01-21 20:09:18 +0530

    Modified Ordets.routes

--- ecommerce-api/routes/orders.routes.js
+++ ecommerce-api/routes/orders.routes.js
@@ -24,4 +24,47 @@ router.post("/",(req,res)=>{
     writeDB(db);
     res.status(201).json(order);
 });
+router.get("/",(req,res)=>{
+    const db=readDB();
+    res.status(200).json({
+        count:db.orders.length,
+        orders:db.orders
+    });
+});
+router.delete("/:order_id",(req,res)=>{
+    const db=readDB();
+    const order=db.orders.find(o=>o.id==req.params.order_id);
 
+    if(!order)
+        return res.status(404).json({message:"Order not found"});
+    if(order.status==="Cancelled")
+        return res.status(400).json({message:"Order already cancelled"});
+    const today=new Date().totalSOString().split("T")[0];
+    if(order.created_at !==today)
+        return res.status(400).json({message:"Cancellation not allowed"});
+    order.status="Cancelled";
+    const product=db.products.find(p=>p.id===order.product_id);
+    product.stock+=order.quantity;
+    writeDB(db);
+    res.status(200).json({message:"Order cancelled successfully"});
+});
+router.patch("/change/status/:order_id",(req,res)=>{
+    const{status}=req.body;
+    const validStatusFlow=["Placed","Shipped","Dleivered"];
+    const db=readDB();
+    const order=db.orders.find(o=>o.id==req.params.order_id);
+    if(!order)
+        return res.status(404).json({message:"Order not found"});
+    if(order.ststus==="Cancelled"||order.status==="Delivered")
+        return res.status(400).json({message:"Status change not allowed"});
+    const currentIndex=validStatusFlow.indexOf(order.status);
+    const nextIndex=validStatusFlow.indexOf(status);
+
+    if(nextIndex!==currentIndex+1)
+        return res.status(400).json({message:"Invalid ststus flow"});
+
+    order.status=status;
+    writeDB(db);
+    res.status(200).json(order);
+});
+module.exports=router;
